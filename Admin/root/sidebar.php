541059d7c89b91afb6e725608379feefa0b972c5
captainprox <92608546+captainprox02@users.noreply.github.com>
2021-12-07 23:49:39 +0700

    xem mat hang/ xem tong quan
    ui xem mat hang/ xem tong quan
    ngay mai: lam form sua mat hang, them mat hang

--- Admin/root/sidebar.php
+++ Admin/root/sidebar.php
@@ -4,16 +4,16 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./CSS/style.css">
+    <link rel="stylesheet" href="../CSS/style.css">
 </head>
 <body>
 <div class="sidebar-menu">
               <ul class="menu">
                 <li class="menu-item shadow-box">
-                  <a href="./page-1.php" class="menu-link">Tinh nang 1</a>
+                  <a href="./page-1.php" class="menu-link"><i class="far fa-eye"></i>Tổng quan</a>
                 </li>
                 <li class="menu-item shadow-box">
-                  <a href="./page-2.php" class="menu-link">Tinh nang 2</a>
+                  <a href="./page-2.php" class="menu-link"><i class="fas fa-shopping-cart"></i>Các mặt hàng</a>
                 </li>
                 <li class="menu-item shadow-box">
                   <a href="#" class="menu-link">Tinh nang 3</a>
@@ -27,4 +27,5 @@
               </ul>
             </div>
 </body>
+<script src="https://kit.fontawesome.com/cb1ae4cd96.js" crossorigin="anonymous"></script>
 </html>
