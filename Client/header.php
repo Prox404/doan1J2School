3791c7ffbc85471d4ce1cf0161a9d4bd8406ed87
chau <trancongtri008@gmail.com>
2021-12-11 18:34:40 +0700

    update continue

--- Client/header.php
+++ Client/header.php
@@ -9,31 +9,31 @@
     <link rel="stylesheet" href="./css/layout.css">
 </head>
 <body>
-    <div class="header shadow-box">
-        
-          <img
-            src="https://i.ibb.co/6bZRxw4/P-ogrange.png"
-            alt=""
-            class="header-logo"
-          />
-          <input type="search" name="" id="">
-          <ul class="header-menu">
-            <li>
-              <a href="#">Thông báo</a>
-            </li>
-            <li>
-              <a href="#">Hỗ trợ</a>
-            </li>
-            <li>
-              <a href="#">Đăng nhập</a>
-            </li>
-            <li>
-              <a href="./1-form-dang-ky.html">Đăng kí</a>
-            </li>
-          </ul>
-          
-    </div>
-<div id="sale">
+  <div class="header shadow-box">
+    <a href="index.php" class="logo">
+    <img
+      src="https://i.ibb.co/6bZRxw4/P-ogrange.png"
+      alt=""
+      class="header-logo"
+    />
+    </a>
+    <input type="search" value="" name="" id="">
+    <ul class="header-menu">
+      <li>
+        <a href="#">Thông báo</a>
+      </li>
+      <li>
+        <a href="#">Hỗ trợ</a>
+      </li>
+      <li>
+        <a href="#">Đăng nhập</a>
+      </li>
+      <li>
+        <a href="./1-form-dang-ky.html">Đăng kí</a>
+      </li>
+    </ul>  
+  </div>
+  <div id="sale">
     <div class="left" >
         <a href="#">
             <img class="shadow-box" src="https://img.freepik.com/free-vector/creative-sales-banner-with-abstract-details_52683-67038.jpg?size=626&ext=jpg", width="100%", height="100%">
