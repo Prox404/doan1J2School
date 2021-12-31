3c14996ff0d55244ed6c7d641b690289e52653d3
chau <trancongtri008@gmail.com>
2022-01-01 00:08:27 +0700

    fix delete_account.php

--- Client/delete_account.php
+++ Client/delete_account.php
@@ -1,8 +1,10 @@
 <!-- delete acount in php -->
 <?php
+session_start();
 require_once 'connect.php';
 
 $id = $_SESSION['id'];
+echo $id;
 $sql = "DELETE FROM customer WHERE id = $id";
 if (mysqli_query($connect, $sql)) {
     header('location:users.php?success=Delete successful');
