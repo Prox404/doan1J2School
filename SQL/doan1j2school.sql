ae3a39cc069894f1914add2a3604f3a1740ae098
captainprox <92608546+captainprox02@users.noreply.github.com>
2022-01-01 05:12:04 +0700

    xin loi vi da sua sql :((
    Day la lan cuoi thay doi sql :((

--- SQL/doan1j2school.sql
+++ SQL/doan1j2school.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Dec 26, 2021 at 03:43 PM
+-- Generation Time: Dec 31, 2021 at 10:10 PM
 -- Server version: 5.7.33
 -- PHP Version: 7.4.19
 
@@ -35,8 +35,17 @@ CREATE TABLE `bill` (
   `recipient_name` varchar(20) NOT NULL,
   `customer_phone` varchar(20) NOT NULL,
   `customer_address` varchar(30) NOT NULL,
-  `note` varchar(30) NOT NULL
+  `note` varchar(30) NOT NULL,
+  `status` tinyint(4) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `bill`
+--
+
+INSERT INTO `bill` (`id`, `customer_id`, `time_order`, `recipient_name`, `customer_phone`, `customer_address`, `note`, `status`) VALUES
+(1, 1, '2021-12-30 02:47:09', 'Tri dep trai', '012345678', 'Quang Nam', 'Tri rat dep trai', 2),
+(2, 1, '2021-12-30 02:47:31', 'Tri nha giau', '012345678', 'Quang Nam', 'Tri rat dep trai', 2);
 
 -- --------------------------------------------------------
 
@@ -49,6 +58,15 @@ CREATE TABLE `bill_detail` (
   `product_id` int(11) NOT NULL,
   `quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `bill_detail`
+--
+
+INSERT INTO `bill_detail` (`bill_id`, `product_id`, `quantity`) VALUES
+(1, 1, 3),
+(1, 2, 3),
+(1, 3, 2);
 
 -- --------------------------------------------------------
 
@@ -92,8 +110,9 @@ CREATE TABLE `employee` (
 --
 
 INSERT INTO `employee` (`id`, `name`, `phone`, `address`, `gender`, `dob`, `email`, `password`, `level_id`, `token`) VALUES
-(1, 'Tri dep trai', '12345678', 'Quang Nam', b'01', '2021-12-30', 'ecec@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', 1, ''),
-(2, 'Tri nha giau', '12345678', 'Quang Nam', b'00', '2021-12-25', 'ecec@gmail.com', '827ccb0eea8a706c4c34a16891f84e7b', 1, '');
+(1, 'Tri nha giau', '012345678', 'Quang Nam', b'01', '2001-01-01', 'ecec@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', 1, 'ecec@gmail.com81dc9bdb52d04dc20036dbd8313ed055'),
+(2, 'Tri dep trai', '012345678', 'Quang Nam', b'00', '2002-06-29', 'ecec@gmail.com', '81dc9bdb52d04dc20036dbd8313ed055', 1, 'ecec@gmail.com81dc9bdb52d04dc20036dbd8313ed055'),
+(3, 'Tri hoc gioi ', '012345678', 'Quang Nam', b'01', '2003-02-01', 'ecec@gmail.com', '202cb962ac59075b964b07152d234b70', 1, 'ecec@gmail.com202cb962ac59075b964b07152d234b70');
 
 -- --------------------------------------------------------
 
@@ -153,8 +172,9 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`id`, `name`, `description`, `image`, `cost`, `quantity`, `manufacturer_id`, `sold`, `type_id`) VALUES
-(3, 'Áo khoác màu hường nam tính', '31313', '1640303301.png', 13131, 421, 2, 0, 0),
-(5, 'Anh tri dep try qua', '5313', '1640303434.png', 131312, 12, 2, 0, 0);
+(1, 'Áo khoác đen màu hường nam tính', 'Anh Trí đẹp try quá hjhj', '1640843224.png', 200000, 43, 2, 0, 1),
+(2, 'Quần siêu dài 20cm', 'Quần siêu dài ', '1640843275.png', 50000, 35, 5, 0, 1),
+(3, 'Anh người yêu siêu cấp vjp pro', 'Đẹp try, học giỏi, con nhà giàu, body sáu múi là những gì anh ấy méo có', '1640984974.jpg', 3000, 69, 7, 0, 1);
 
 -- --------------------------------------------------------
 
@@ -221,7 +241,7 @@ ALTER TABLE `type`
 -- AUTO_INCREMENT for table `bill`
 --
 ALTER TABLE `bill`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `customer`
@@ -233,7 +253,7 @@ ALTER TABLE `customer`
 -- AUTO_INCREMENT for table `employee`
 --
 ALTER TABLE `employee`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `level`
@@ -251,7 +271,7 @@ ALTER TABLE `manufacturer`
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `type`
