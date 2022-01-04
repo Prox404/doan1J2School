b152ab6ede8d6d8d50353aa6beecb3292e8b5ef6
chau <trancongtri008@gmail.com>
2022-01-04 16:58:49 +0700

    update validate signup form

--- Client/process_signup.php
+++ Client/process_signup.php
@@ -1,42 +1,40 @@
 <?php
-
 session_start();
-$name = $_POST['name'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-$re_password = $_POST['re-password'];
-$token = $name.'td5'.$password;
+// Connect to database
 
 require 'connect.php';
-$sql = "SELECT count(*) FROM customer WHERE email = '$email'";
+
+if ($connect->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+// create token
+$token = md5(uniqid(rand(), true));
+
+// Insert data into database
+$sql = "SELECT * FROM customer WHERE email = '$email'";
 $result = mysqli_query($connect, $sql);
-$number_row = mysqli_fetch_array($result)['count(*)'];
+$number_row = mysqli_num_rows($result);
 
+// check if email is already exist
 if ($number_row == 1) {
-    header('location:signup.php?error=email_exist');
-    exit;
-}
-
-$sql = "INSERT INTO customer(name,email,password,token,dob,phone,address) VALUES ('$name', '$email', '$password', '$token',CURRENT_DATE, '', '')";
-if (mysqli_query($connect, $sql)) {
-    $sql = "SELECT * FROM customer WHERE email = '$email'";
-    $result = mysqli_query($connect, $sql);
-    $id = mysqli_fetch_array($result)['id'];
-    $_SESSION = [
-        'loggedin' => true,
-        'id' => $id,
-        'name' => $name,
-        'email' => $email,
-        'token' => $token
-    ];
-    header('location:index.php?success=signup_success');
-    exit;
+    header('location:signup.php?error=email_existed');
 } else {
-    die('Error: ' . mysqli_error($connect));
-    header('location:signup.php?error=signup_fail');
-    exit;
+    $sql = "INSERT INTO customer(name,email,password,token,dob,phone,address) VALUES ('$name', '$email', '$password', '$token',CURRENT_DATE, '', '')";
+    if (mysqli_query($connect, $sql)) {
+        $sql = "SELECT * FROM customer WHERE email = '$email'";
+        $result = mysqli_query($connect, $sql);
+        $id = mysqli_fetch_array($result)['id'];
+        $_SESSION = [
+            'loggedin' => true,
+            'id' => $id,
+            'name' => $name,
+            'email' => $email,
+            'token' => $token
+        ];
+        header('location:index.php?success=signup_success');
+    } else {
+        die('Error: ' . mysqli_error($connect));
+        header('location:signup.php?error=signup_fail');
+    }
 }
-
-
-
 mysqli_close($connect);
--- Client/signup.php
+++ Client/signup.php
@@ -12,32 +12,103 @@
 </head>
 
 <body>
+    <?php
+    // define variables and set to empty values
+    $name = $email = $password = $re_password = "";
+    $nameErr = $emailErr = $passwordErr = $re_passwordErr = "";
+
+    // Input field validation
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // String validation
+        if (empty($_POST["name"])) {
+            $nameErr = "Tên không được để trống";
+        } else {
+            $name = test_input($_POST["name"]);
+            // check if name only contains letters and whitespace
+            if (!preg_match("/^[a-zA-Z ]*$/", $name)) {
+                $nameErr = "Tên chỉ được chứa chữ cái và khoảng trắng";
+            }
+        }
+
+        if (empty($_POST["email"])) {
+            $emailErr = "Email không được để trống";
+        } else {
+            $email = test_input($_POST["email"]);
+            // check if e-mail address is well-formed
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $emailErr = "Email không hợp lệ";
+            }
+        }
+
+        if (empty($_POST["password"])) {
+            $passwordErr = "Mật khẩu không được để trống";
+        } else {
+            $password = test_input($_POST["password"]);
+            // check if password only contains letters and numbers
+            if (!preg_match("/^[a-zA-Z0-9]*$/", $password)) {
+                $passwordErr = "Mật khẩu chỉ được chứa chữ cái và số";
+            }
+        }
+
+        if (empty($_POST["re-password"])) {
+            $re_passwordErr = "Nhập lại mật khẩu không được để trống";
+        } else {
+            $re_password = test_input($_POST["re-password"]);
+            // check if password only contains letters and numbers
+            if (!preg_match("/^[a-zA-Z0-9]*$/", $re_password)) {
+                $re_passwordErr = "Nhập lại mật khẩu chỉ được chứa chữ cái và số";
+            }
+        }
+
+        // Check if password and re-password are the same
+        if ($password != $re_password) {
+            $re_passwordErr = "Mật khẩu không trùng khớp";
+        }
+    }
+    function test_input($data)
+    {
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    } ?>
     <div id="container">
         <?php include 'header.php'; ?>
         <div class="signup">
             <div class="signup-form">
                 <h2 class="form-title">Đăng ký</h2>
-                <form method="POST" class="signUp" id="signUp" action="process_signup.php" onsubmit="return validateFormSignUp()">
+                <form method="POST" class="signUp" id="signUp" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
                     <div class="form-group">
                         <label for="name">Tên:</label>
                         <input type="text" name="name" id="name" placeholder="Họ và tên" />
+                        <span class="error"><?php echo $nameErr; ?></span>
                     </div>
                     <div class="form-group">
                         <label for="email">Email:</label>
                         <input type="email" name="email" id="email" placeholder="Email" />
+                        <span class="error"><?php echo $emailErr; ?></span>
                     </div>
                     <div class="form-group">
                         <label for="pass">Mật khẩu:</label>
                         <input type="password" name="password" id="password" placeholder="Mật khẩu" />
+                        <span class="error"><?php echo $passwordErr; ?></span>
                     </div>
                     <div class="form-group">
                         <label for="re-pass">Nhập lại mật khẩu:</label>
                         <input type="password" name="re-password" id="re-password" placeholder="Nhập lại mật khẩu" />
-                    <div class="form-group form-button">
-                        <input type="submit" name="signup" id="signup" class="form-submit" value="Đăng ký" />
-                    </div>
+                        <span class="error"><?php echo $re_passwordErr; ?></span>
+                        <div class="form-group form-button">
+                            <input type="submit" name="signup" id="signup" class="form-submit" value="Đăng ký" />
+                        </div>
                 </form>
             </div>
         </div>
     </div>
+    <?php
+    if (isset($_POST['signup'])) {
+        if ($nameErr == "" && $emailErr == "" && $passwordErr == "" && $re_passwordErr == "") {
+            include 'process_signup.php';
+        }
+    }
+    ?>
 </body>
